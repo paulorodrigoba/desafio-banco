ed4b16d75594aa01533f958dfdb992f937b0ae61
Paulo Rodrigo <paulorodrigoba@outlook.com>
2024-06-03 16:22:46 -0400

    Primeiro commit

---
+++ banco.py
@@ -0,0 +1,63 @@
+import datetime
+
+menu = """
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 5
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            data_hora = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            extrato += f"{data_hora} - Depósito: R$ {valor:.2f}\n"
+            print(f"Depósito de R$ {valor:.2f} realizado com sucesso!")
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "s":
+        if numero_saques >= LIMITE_SAQUES:
+            print("Operação falhou! Número máximo de saques diários excedido.")
+            continue
+
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+        elif valor > 0:
+            saldo -= valor
+            numero_saques += 1
+            data_hora = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            extrato += f"{data_hora} - Saque: R$ {valor:.2f}\n"
+            print(f"Saque de R$ {valor:.2f} realizado com sucesso!")
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "e":
+        print("\n================ EXTRATO ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == "q":
+        print("Obrigado por utilizar o sistema bancário. Até logo!")
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
